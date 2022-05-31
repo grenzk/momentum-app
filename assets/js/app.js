4c297f1940fe9c8f01921eda76a11e04579c9cbc
grenzk <74033991+grenzk@users.noreply.github.com>
2022-05-31 15:16:48 +0800

    Add clock text

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,9 @@
 const hour = document.getElementById('clock-hour')
 const minutes = document.getElementById('clock-minutes')
 const seconds = document.getElementById('clock-seconds')
+const textHour = document.getElementById('text-hour')
+const textMinutes = document.getElementById('text-minutes')
+const textAmPm = document.getElementById('text-ampm')
 
 const clock = () => {
   let date = new Date()
@@ -14,3 +17,39 @@ const clock = () => {
   seconds.style.transform = `rotateZ(${ss}deg)`
 }
 setInterval(clock, 1000)
+
+const clockText = () => {
+  let date = new Date()
+
+  let hh = date.getHours(),
+    ampm,
+    mm = date.getMinutes(),
+    day = date.getDate(),
+    month = date.getMonth(),
+    year = date.getFullYear()
+
+  if (hh >= 12) {
+    hh = hh - 12
+    ampm = 'PM'
+  } else {
+    ampm = 'AM'
+  }
+
+  if (hh == 0) {
+    hh = 12
+  }
+
+  if (hh < 10) {
+    hh = `0${hh}`
+  }
+
+  textHour.textContent = `${hh}:`
+
+  if (mm < 10) {
+    mm = `0${mm}`
+  }
+
+  textMinutes.textContent = mm
+  textAmPm.textContent = ampm
+}
+setInterval(clockText, 1000)

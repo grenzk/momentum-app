75025d37c0690ed291c93a5c349e15aa97a4c9d4
grenzk <74033991+grenzk@users.noreply.github.com>
2022-06-03 23:58:04 +0800

    Add dark mode

--- assets/js/buttons.js
+++ assets/js/buttons.js
@@ -4,13 +4,6 @@ const todoList = document.querySelector('#todo-container')
 function toggleTodo() {
   todoList.classList.toggle('show')
   todoList.classList.toggle('hide')
-  if (todoList.classList.contains('hide')) {
-    setTimeout(() => {
-      todoList.style.left = '-100%'
-    }, 500)
-  } else {
-    todoList.style.left = ''
-  }
 }
 
 todoBtn.addEventListener('click', toggleTodo)
